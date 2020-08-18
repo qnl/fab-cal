90525d83f793ec1d56ff50cda4880cd6dae6d0f4
Larry Chen <larrychen@berkeley.edu>
2020-08-18 12:11:25 -0700

    Updated project name

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
-  publicPath: process.env.NODE_ENV === 'production' ? '/fab/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/fab-cal/' : '/'
 };
